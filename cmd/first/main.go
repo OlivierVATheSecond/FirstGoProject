d166776974cf48587c3a722998aa8d9f197f71d0
Olivier van Asperen <oliviervanasperen@gmail.com>
2020-11-01 14:10:14 +0100

    addition calculator

--- cmd/first/main.go
+++ cmd/first/main.go
@@ -9,5 +9,15 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Print("First num: ")
+	var a int
+	fmt.Scanln(&a)
+
+	fmt.Print("Second num: ")
+	var b int
+	fmt.Scanln(&b)
+
+	var c int = a + b
+
+	fmt.Println("Output:", c)
 }
